82d003986fd91cd85d8a725894b8a9558bd5f291
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-04-26 14:01:02 -0700

    set up Budo server and started data files

---
+++ public/scripts/app.js
@@ -0,0 +1,30 @@
+/* CLIENT-SIDE JS
+ *
+ * This is your main angular file. Edit as you see fit.
+ *
+ */
+
+angular
+  .module('tunely', ['ngRoute'])
+  .config(config);
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+
+function config ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/templates/albums',
+      controllerAs: 'albumsIndexCtrl',
+      controller: 'AlbumsIndexController'
+    })
+    .when('/albums/:id', {
+      templateUrl: '/templates/albums-show',
+      controllerAs: 'albumsShowCtrl',
+      controller: 'AlbumsShowController'
+    })
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+  });
+}
